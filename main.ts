16a6dad1dc9493cea0f68498d41a85654043b168
attilamakerhu <87415155+attilamakerhu@users.noreply.github.com>
2022-07-15 15:03:45 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+radio.onReceivedValue(function (name, value) {
+    if (value == 1) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            . . . . .
+            `)
+    }
+})
+radio.setGroup(43)
 basic.forever(function () {
-	
+    if (input.isGesture(Gesture.Shake)) {
+        radio.sendValue("shake", 1)
+    } else {
+        radio.sendValue("shake", 0)
+    }
 })
